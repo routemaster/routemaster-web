cb21e4b83dcaa8fa89339bb2564a595c0bf7e4e4
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2013-02-22 21:26:05 -0500

    Enable logging, and use log4j
    log4j is Apache's logger. It gives us tons of stuff we'll never use, but
    it's simple enough to install with Maven, so why not?

--- src_java/routemaster/JettyServer.java
+++ src_java/routemaster/JettyServer.java
@@ -5,11 +5,12 @@ import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.util.log.Log;
-import org.eclipse.jetty.util.log.Logger;
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
 
 public class JettyServer extends Server {
-    //private static final Logger LOG = Log.getLogger(JettyServer.class);
+    private static final Logger log = Logger.getLogger(JettyServer.class);
     ResourceHandler resourceHandler = new ResourceHandler();
 
     public JettyServer() {
@@ -27,7 +28,7 @@ public class JettyServer extends Server {
         // Use the resources packaged into our jarfile
         resourceHandler.setResourceBase(
             getClass().getClassLoader().getResource("").toExternalForm());
-        //LOG.info("serving " + resourceHandler.getBaseResource());
+        log.info("Configured to Serve " + resourceHandler.getBaseResource());
 
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[] {
@@ -36,6 +37,12 @@ public class JettyServer extends Server {
     }
 
     public static void main(String[] args) throws Exception {
+        // Configure log4j
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.WARN); // for everyone else
+        Logger.getLogger("routemaster").setLevel(Level.INFO); // for us
+
+        // Bind to a port and serve pages up as requested
         JettyServer server = new JettyServer(
             args.length == 0 ? 8080 : Integer.parseInt(args[0]));
         server.start();
