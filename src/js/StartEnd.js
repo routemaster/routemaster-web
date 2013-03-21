b1c6bdc5a83b8d2487929c753f0637fa22830f7d
Michael Retherford <primasecunda@gmail.com>
2013-03-20 23:49:01 -0400

    Added score stuff to Colin's branch

--- src/js/StartEnd.js
+++ src/js/StartEnd.js
@@ -1,18 +1,18 @@
-/*global window, Backbone, _, $, console, L*/
+/*global window, Backbone, _, $, console*/
 $(function() {
     "use strict";
 
     var GpsTracker = Backbone.Model.extend({
         defaults: {
+		    posList: [],
+		    totalDist: undefined,
+		    score: undefined,
             isTracking: false,
             startTime: undefined,
             updatedTime: undefined,
             watchPositionId: undefined, // unique id given by watchPosition()
             position: undefined,
-            lastError: undefined,
-            efficiency: undefined,
-            distance: undefined,
-            map: undefined
+            lastError: undefined
         },
 
         initialize: function() {
@@ -24,18 +24,6 @@ $(function() {
         startTracking: function() {
             var now = Date.now();
             if(this.get("isTracking")) { return; }
-            var osmUrl = 'http://{s}.tile.openstreeetmap.org/{z}/{x}/{y}.png';
-            var osmAttrib = 'Map data © OpenStreetMap contributors';
-            var osm = L.TileLayer(osmUrl, {
-                attribution: osmAttrib,
-                maxZoom: 12,
-                minZoom: 8
-            });
-            var map = L.Map('map', {
-                layers: [osm],
-                zoom: 12,
-                center: [51.505, -0.09]
-            });
             this.set({
                 isTracking: true,
                 startTime: now,
@@ -45,10 +33,7 @@ $(function() {
                     _.bind(this.updatePosition, this),
                     _.bind(this.updatePositionError, this),
                     {enableHighAccuracy: true}
-                ),
-                efficiency: 91,
-                distance: 0.7,
-                map: map
+                )
             });
         },
 
@@ -60,6 +45,8 @@ $(function() {
 
         updatePosition: function(position) {
             // called via navigator.geolocation.watchPosition
+		    posList.push(position);
+		    this.updateScore();
             this.set({
                 position: position,
                 updatedTime: Date.now()
@@ -75,7 +62,7 @@ $(function() {
         GpsView = Backbone.View.extend({
 
         el: $("#gps-status"),
-        template: _.template($("#status-tmpl").html()),
+        template: _.template($("#gps-status-tmpl").html()),
         model: new GpsTracker(),
         events: {
             "click #start-button": "startTracking",
@@ -89,7 +76,38 @@ $(function() {
             );
             this.render();
         },
+		
+		
+		//note that for small distances, pythagorean estimate can suffice
+		//this calculation works on a spherical assumption, see haversine formula
+		calcDist: function(pos1,pos2) {
+		    var lat1 = pos1.coords.latitude;
+		    var lat2 = pos2.coords.latitude;
+		    var lon1 = pos1.coords.longitude;
+		    var lon2 = pos2.coords.longitude;
+			
+		    var R = 6371; // radius of earth in km
+		    var dLat = (lat2-lat1).toRad();
+		    var dLon = (lon2-lon1).toRad();
+		    var lat1 = lat1.toRad();
+		    var lat2 = lat2.toRad();
 
+		    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+		    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		    var d = R * c;
+		    return d;
+		},
+		
+		updateScore: function(){
+		    if (posList.length<=2){score=100; return}//calculation would work fine at length=2 but would be 100 anyway
+		    totalDist = totalDist + calcDist(posList[posList.length-1],posList[posList.length-2]);
+		    var straightDist = calcDist(posList[0],posList[posList.length-1]);
+		    score: 100 * (straightDist/totalDist) * (straightDist/totalDist);
+		},
+			
+			
+		
         render: function() {
             var state = _.extend(_.clone(this.model.attributes), {
                 formatTime: _.bind(this.formatTime, this)
